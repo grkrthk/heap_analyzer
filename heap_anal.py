7804835c8cc744d038d80f9d9cdc57fcf1ab232a
grkrthk <grkrthk@yahoo.co.in>
2014-02-25 16:18:53 -0600

    Added append to the same key but not working

--- heap_anal.py
+++ heap_anal.py
@@ -1,16 +1,18 @@
 import re
 # open the file that has the page
 fpage = open('./4k_block.txt', 'r')
+
+page_name=""
 pagetables = dict();
 count = 0
 for line in fpage:
          # split the line into words
          wordList = re.sub("[^\w]", " ",  line).split()
          refw = wordList[0]
+
          if count == 0 :
                 page_name = refw
-                print page_name
-
+             
          ptr1 = wordList[2]+wordList[1]
          ptr2 = wordList[4]+wordList[3]
 
@@ -19,21 +21,22 @@ for line in fpage:
          subaddr = refw[0:5]  # get the lower order address (it should occur in the lower address range
          ptr1addr = ptr1[4:9] # get the lower order address to compare for the first pointer
          ptr2addr = ptr2[4:9] # get the lower order address to compare for the second pointer         
+       
+         print page_name
 
          if ptr1addr in subaddr :
-                # store it in the page related data structure
-                pagetables[page_name] = ptr1
-                     
+                # store it in the page related to it's data structure
+                pagetables[page_name].append(ptr1)
+                                     
          if ptr2addr in subaddr :
-                pagetables[page_name] = ptr2
-
- 	 values = pagetables.values()
-         print values
+                # store it in the page related to it's data structure
+                pagetables[page_name].append(ptr2)
+                
+ 	 #values = pagetables.get(page_name)
+         #print values
          # increment the count after every line is processed
          count = count + 1
-            
 
-
-
-
+values = pagetables.values()
+print values
 
